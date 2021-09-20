1d1f9e85ca944e78ce7baa05ea250f41f6b8802a
Jakub Dudek <jakub.dudek94@gmail.com>
2021-09-20 16:24:08 +0200

    6.2 Useing TDD method

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(2, 2);
+        int resultSubstract = calculator.subtract(2, 2);
+
+        if (resultAdd == 4) {
+            System.out.println("Dodawanie wykonano poprawnie: " + resultAdd);
+        } else {
+            System.out.println("ERROR: Wynik jest inny od zakładanego, tj. 4");
+        }
+
+        if (resultSubstract == 0) {
+            System.out.println("Odejmowanie wykonano poprawnie: " + resultSubstract);
+        } else {
+            System.out.println("ERROR: Wynik jest inny od zakładanego (tj. 0)");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+
+
+}

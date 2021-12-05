a1765f459a50a702f42be26b93e717ee3ac795f2
Jakub Dudek <jakub.dudek94@gmail.com>
2021-12-05 19:57:46 +0100

    Module 14: Study stuff vol. 2

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.library;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,9 +9,15 @@ import java.util.List;
 public final class Library {
 
     private final List<String> books = new ArrayList<>();
-
-    @Autowired
     private LibraryDbController libraryDbController;
+
+    public Library(final LibraryDbController libraryDbController) {
+        this.libraryDbController = libraryDbController;
+    }
+
+    public Library() {
+
+    }
 
     public void saveToDb() {
         libraryDbController.saveData();
--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class LibraryTestSuite {
 
     @Test
@@ -30,5 +32,17 @@ public class LibraryTestSuite {
 
         //Then
         //do nothing
+    }
+
+    @Test
+    public void testContext() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+        //When & Then
+        System.out.println("===== Beans list: ===== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ===== Beans list =====");
     }
 }

18aa7ccacf870965f863256570e435c0ed29fe38
Jakub Dudek <jakub.dudek94@gmail.com>
2021-12-07 11:33:27 +0100

    14.4 Task: wszystko w jednym (Spring Web) #1

---
+++ kodilla-sping-web/src/main/java/com/kodilla/spring/web/KodillaSpingWebApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaSpingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaSpingWebApplication.class, args);
+    }
+
+}

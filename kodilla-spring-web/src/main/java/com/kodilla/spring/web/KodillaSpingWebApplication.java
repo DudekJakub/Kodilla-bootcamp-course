f3e8a5d21ced16a4656c9a03dbe80e945337a44e
jakub <jakub.dudek94@gmail.com>
2021-12-21 17:02:01 +0100

    SpringWeb: UPDATE

--- kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpingWebApplication.java
+++ kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpingWebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KodillaSpingWebApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(KodillaSpingWebApplication.class, args);
     }
